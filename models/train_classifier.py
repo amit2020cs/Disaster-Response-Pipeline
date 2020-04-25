20d22941fe7c6881f651878c711e6e0e8c1f38cb
Amit Singh <31853896+amit2020cs@users.noreply.github.com>
2020-04-25 23:08:10 +0530

    model

---
+++ models/train_classifier.py
@@ -0,0 +1,82 @@
+import sys
+import pandas as pd
+import nltk
+import re
+from sqlalchemy import create_engine
+import pickle
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.model_selection import GridSearchCV
+
+
+
+def load_data(database_filepath):
+    # load data from  database  as dataframe
+    # X : messages(features)
+    # Y:  categories(target)
+    # categories name
+    
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('DiasasterMessages',engine)
+    X = df['messages']
+    Y = df.iloc[:,4:]
+    category_name = list(df.columns[4:])
+    
+    return X,Y, category_names
+
+
+def tokenize(text):
+    
+    pass
+
+
+def build_model():
+    pass
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    pass
+
+
+def save_model(model, model_filepath):
+    pass
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
